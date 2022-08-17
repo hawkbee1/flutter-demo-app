18b8aa08b3a53917e70bb2c4472b2cf73abd0ac7
hawkbee1 <googandads@gmail.com>
2022-08-17 12:48:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+![alt text](https://i.imgur.com/cOj85Lg.jpg "Passbase Banner")
+
+# Official Passbase Flutter Demo App
+
+This App shows an example integration of the Passbase Flutter package integrated into an App. Before your try to run the App please sign up on our [developer platform](https://app.passbase.com/signup) and use **your own publishabe API key**, which you can find in the [API settings](https://app.passbase.com/settings/api) section. 
+
+Please follow our integration guide in our [official developer documentation](https://docs.passbase.com/integrations/flutter) to install all dependencies first in a correct way.
+
+## Requirements
+
+Ensure that you are using an up to date version for Flutter. You will also need the latest Xcode with Swift 5.1 support and [Cocoapods](https://cocoapods.org/) for dependencies. For Android please use Android Studio and ensure that you have Kotlin support.
+
+## Install the App & Run
+
+You can follow our [integration guide](https://docs.passbase.com/integrations/flutter) to correctly install the Passbase Package. To run the Demo app please do the folllwing:
+
+1. Open Terminal in project folder and run `flutter packages get`
+2. Exchange `YOUR_OWN_PUBLISHABLE_API_KEY` with your own API key in the file `main.dart` line 26 in the lib folder.
+3. Build and run the App
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import 'package:passbase_flutter/passbase_flutter.dart';
+
+void main() => runApp(PassbaseFlutterDemoApp());
+
+class PassbaseFlutterDemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    PassbaseSDK.prefillUserEmail = "googandads@gmail.com";
+    PassbaseSDK.initialize(
+        publishableApiKey:
+            "Ww3TIde3B8nh5M3EZ57tkzIbMyAVwzh9YzW8FcwADKgpQ76UfT5bqox2dvdNTDVo");
+    return MaterialApp(
+      home: PassbaseDemoHomePage(),
+    );
+  }
+}
+
+class PassbaseDemoHomePage extends StatefulWidget {
+  PassbaseDemoHomePage({Key? key}) : super(key: key);
+
+  @override
+  _PassbaseDemoHomePageState createState() {
+    return _PassbaseDemoHomePageState();
+  }
+}
+
+class _PassbaseDemoHomePageState extends State<PassbaseDemoHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 50, 107, 236),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 80),
+              child: Image.asset(
+                'lib/assets/shield-icon.png',
+                width: 50,
+                height: 50,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 45),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Welcome to Passbase',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontFamily: 'Exo',
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    'Start your first verification by clicking on the button below.',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontFamily: 'Exo',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 70),
+              child: PassbaseButton(
+                onFinish: (identityAccessKey) {
+                  // do stuff in case of success
+                  print(identityAccessKey);
+                },
+                onError: (errorCode) {
+                  // do stuff in case of cancel
+                  print(errorCode);
+                },
+                onStart: () {
+                  // do stuff in case of start
+                  print("start");
+                },
+                width: 300,
+                height: 60,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
